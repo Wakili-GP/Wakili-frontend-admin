bc2c62fdfd49245cda0a98586b5b061c70355c27
Usama <uelareeny@gmail.com>
2026-02-05 11:36:51 +0200

    added the user management page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Login from "./AdminLogin";
 import AdminDashboard from "./admin/AdminDashboard";
 import LawyerVerification from "./admin/LawyerVerification";
 import CredentialReview from "./admin/CredentialReview";
+import UserManagement from "./admin/UserManagement";
 import NotFound from "./NotFound";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -19,6 +20,7 @@ function App() {
             <Route path="dashboard" element={<AdminDashboard />} />
             <Route path="verification" element={<LawyerVerification />} />
             <Route path="credentials" element={<CredentialReview />} />
+            <Route path="users" element={<UserManagement />} />
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
